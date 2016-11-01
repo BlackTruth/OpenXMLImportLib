183af063b49afb9e574b8e6f6c8a734699b23bcd
BlackTruth <ivc.kulagin@gmail.com>
2016-11-01 14:41:08 +0300

    Без форматирования и регистрации SDK

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -53,26 +53,33 @@ namespace ConsoleApplication1
 
         static void AddExcel(string path)
         {
-            //for (int i = 1; i < 8; i++)
-            //{
-            //    for (int j = 1; j < 8; j++)
-            //    {
-            //        OpenXMLImportDLL.ExcelImport.AddCellData(i, j, "QWdasd111");
+            for (int i = 1; i < 8; i++)
+            {
+                for (int j = 1; j < 8; j++)
+                {
+                    OpenXMLImportDLL.ExcelImport.AddCellData(i, j, (i+j).ToString());
 
-            //    }
-            //}
-            OpenXMLImportDLL.ExcelImport.AddCellData(1, 1, "1");
-            OpenXMLImportDLL.ExcelImport.AddCellData(1, 2, "2");
-            OpenXMLImportDLL.ExcelImport.AddCellData(1, 3, "3");
-            OpenXMLImportDLL.ExcelImport.AddCellData(1, 4, "4");
-            OpenXMLImportDLL.ExcelImport.AddCellData(2, 1, "1,1");
-            OpenXMLImportDLL.ExcelImport.AddCellData(2, 2, "2,2");
-            OpenXMLImportDLL.ExcelImport.AddCellData(2, 3, "3,3");
-            OpenXMLImportDLL.ExcelImport.AddCellData(2, 4, "4,4");
-            OpenXMLImportDLL.ExcelImport.AddCellData(3, 1, "=A1+A2");
-            OpenXMLImportDLL.ExcelImport.AddCellData(3, 2, "=B1+B2");
-            OpenXMLImportDLL.ExcelImport.AddCellData(3, 3, "=C1+C2");
-            OpenXMLImportDLL.ExcelImport.AddCellData(3, 4, "=D1+D2");
+                }
+            }
+            OpenXMLImportDLL.ExcelImport.AddCellData(8, 1, "=A1+A2+A3+A4+A5+A6+A7");
+            OpenXMLImportDLL.ExcelImport.AddCellData(8, 2, "=B1+B2+B3+B4+B5+B6+B7");
+            OpenXMLImportDLL.ExcelImport.AddCellData(8, 3, "=C1+C2+C3+C4+C5+C6+C7");
+            OpenXMLImportDLL.ExcelImport.AddCellData(8, 4, "=D1+D2+D3+D4+D5+D6+D7");
+
+            OpenXMLImportDLL.ExcelImport.AddCellData(8, 5, "=E1+E2+E3+E4+E5+E6+E7");
+            OpenXMLImportDLL.ExcelImport.AddCellData(8, 6, "=F1+F2+F3+F4+F5+F6+F7");
+            OpenXMLImportDLL.ExcelImport.AddCellData(8, 7, "=G1+G2+G3+G4+G5+G6+G7");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(1, 2, "2");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(1, 3, "3");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(1, 4, "4");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(2, 1, "1,1");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(2, 2, "2,2");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(2, 3, "3,3");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(2, 4, "4,4");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(3, 1, "=A1+A2");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(3, 2, "=B1+B2");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(3, 3, "=C1+C2");
+            //OpenXMLImportDLL.ExcelImport.AddCellData(3, 4, "=D1+D2");
 
             OpenXMLImportDLL.ExcelImport.GenerateExcel(path + "/Newtest.xlsx");
             OpenXMLImportDLL.ExcelImport.ClearArray();
